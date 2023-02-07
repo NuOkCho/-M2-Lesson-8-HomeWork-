5c9354c6c297a68dfed5f6e8a8fe29abdc7fffc9
Ahmed Muvaza <makbookicloud312@mail.ru>
2023-02-07 15:54:55 +0600

    Initial Commit

---
+++ (M2)Lesson-8(HomeWork)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  (M2)Lesson-8(HomeWork)
+//
+//  Created by Ahmed Muvaza on 7/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
